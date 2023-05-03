ba650a1cba1ed1cbe39732ff8db504f99c396eb0
IBM-omr <131830199+IBM-omr@users.noreply.github.com>
2023-05-03 23:34:14 +0100

    Add files via upload

---
+++ exo_02.rb
@@ -0,0 +1,5 @@
+puts 'Bonjour, monde !'
+print 'Et avec une voix sexy, ça donne : Bonjour, monde !'
+
+# PUTS, apres avoir afficher la chaine qu'on lui passe il ajoute un retour a la ligne
+# Alors que PRINT affiche la chaine sans un retour a la ligne
